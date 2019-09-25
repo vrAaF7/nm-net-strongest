c16c18493d4622153254fe8818859a7074bedb8a
Dom Rodriguez <shymega@shymega.org.uk>
2019-09-25 19:52:46 +0100

    [codebase]: Expand main.c
    This is an expansion of the code in main.c - I aim to split it out into
    different files, and add small, efficient functions for certain parts of
    the program.
    
    So far, this change does the following:
    
    - Connects to NetworkManager.
    - Gets an array of active connections from NetworkManager.
    - Finds the wireless (Wi-Fi) connection and uses it.
    - Requests a SSID scan.
    - Waits for the results of SSID scan (this is a hack - going to use
      async callback from libnm in future changes! TODO).
    - Iterates over results of SSID scan, and print the SSID, UUID, ID, and
    - WAP BSSID.
    
    That's pretty much it.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,24 @@
-#include <glib-2.0/glib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <glib-2.0/glib.h>
 #include <glib-2.0/gio/gio.h>
+
 #include <libnm/NetworkManager.h>
 #include <NetworkManager.h>
+
+#include <string.h>
 
 int
 main (void) {
     NMClient *client;
     GError *error = NULL;
     const GPtrArray *conns;
-    int i;
+    guint i;
 
     if (! (client = nm_client_new (NULL, &error))) {
-        g_message("Error: Could NOT connect to NetworkManager: %s.",
+        g_message ("Error: Could NOT connect to NetworkManager: %s.",
             error->message);
         g_error_free (error);
         return EXIT_FAILURE;
@@ -25,25 +30,64 @@ main (void) {
     }
 
     conns = nm_client_get_active_connections (client);
-    printf("Connections:\n============\n");
+    printf ("Connections:\n============\n");
 
-    for (i = 0; i < (int) conns->len; i++) {
+    for (i = 0; i < conns->len; i++) {
         NMActiveConnection *conn;
-        const char *uuid, *type, *id;
+        const char *uuid, *id;
         const GPtrArray *devices;
         NMDevice *device;
+        const GPtrArray *accessPoints;
+        guint ap;
 
         conn = conns->pdata[i];
         uuid = nm_active_connection_get_uuid (conn);
-        type = nm_active_connection_get_connection_type (conn);
         id = nm_active_connection_get_id (conn);
 
         devices = nm_active_connection_get_devices (conn);
         device = g_ptr_array_index (devices, 0);
 
         if (NM_IS_DEVICE_WIFI (device)) {
-            printf("UUID: %s | Name: %s | Type: %s\n", uuid, id, type);
+            nm_device_wifi_request_scan(
+                ((NMDeviceWifi *) device),
+                NULL,
+                NULL);
+
+            printf("Sleeping to allow NetworkManager to update.\n");
+            sleep(2); /* we should wait for NM to update */
+
+            accessPoints = nm_device_wifi_get_access_points (NM_DEVICE_WIFI (device));
+
+            for (ap = 0; ap < accessPoints->len; ap++) {
+                NMAccessPoint *accessPoint;
+                GBytes *accessPoint_SSID_GB;
+                gsize len;
+                guint8 *bytes;
+                char *accessPoint_SSID;
+
+                accessPoint = accessPoints->pdata[ap];
+                accessPoint_SSID_GB = nm_access_point_get_ssid (accessPoint);
+                if (accessPoint_SSID_GB == NULL) {
+                    /* this is OK to have as a value
+                     *
+                     * we just silently handle it, and
+                     * iterate to the next element in
+                     * the array. */
+                    continue;
+                }
+
+                bytes = (guint8*)g_bytes_unref_to_data (accessPoint_SSID_GB, &len);
+                accessPoint_SSID = nm_utils_ssid_to_utf8 (bytes, len);
+
+                printf ("SSID: %s | UUID: %s | ID: %s | WAP BSSID: %s\n",
+                        accessPoint_SSID,
+                        uuid,
+                        id,
+                        nm_access_point_get_bssid (accessPoint));
+            }
+
         }
+
     }
 
     g_object_unref (client);
